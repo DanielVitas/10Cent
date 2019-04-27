eb802891dd020d56b096e3dfcc37e63882b3d2ef
Yaneh <janezic.mj@gmail.com>
2019-04-27 11:56:06 +0200

    pc merge

---
+++ 10Cent/src/audio/AudioPlayer.java
@@ -0,0 +1,11 @@
+package audio;
+
+import javafx.application.Application;
+
+public enum AudioPlayer {
+    TEST("test.mp3");
+    static final String PATH = "";
+    AudioPlayer(String string){
+
+    }
+}
